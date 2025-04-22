0a5bb41b878f4e88967378cce6d226c04d14074e
Souvik Khanagwal <mba24218@iimsirmaur.ac.in>
2025-04-22 09:19:36 +0000

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ const createDeck = () => {
 };
 
 // Function to shuffle the deck using Fisher-Yates algorithm
-const shuffleDeck = (deck) => {
+const shuffleDeck = (deck: { suit: string; value: string }[]) => {
   for (let i = deck.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [deck[i], deck[j]] = [deck[j], deck[i]];
